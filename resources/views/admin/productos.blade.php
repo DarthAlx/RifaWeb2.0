37d3b858522628067d259936e1e61813805c9d12
Alexis Morales <alx.morales@hadoukendev.com>
2018-01-15 14:54:08 -0600

    Link productos

--- resources/views/admin/productos.blade.php
+++ resources/views/admin/productos.blade.php
@@ -4,10 +4,21 @@
 <div class=" main">
 	<div class="container-fluid">
 		<div class="row">
+			<div class="col-md-6">
+				<h3 class="">Añadir nuevo</h3>
+			</div>
+			<div class="col-md-6 text-right valign-wrapper" style="justify-content: space-between;">
+				<div class="text-center" style="margin-left: auto;">
+					<input type="submit" value="Guardar" class="btn btn-primary right waves-effect waves-light btn-large">
+				</div>
+				
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-md-8">
 				<div class="card">
 				  <div class="card-body">
-				    <h5 class="card-title">Añadir nuevo</h5>
+				    <h5 class="card-title">Detalles</h5>
 				    <div class="card-text">
 				    	<form class="col s12">
 					      <div class="row">
@@ -27,17 +38,6 @@
 					        </div>
 					      </div>
 					      <div class="row">
-					        <div class="input-field col col-md-6">
-					          <input id="precio" type="text" class="validate" required>
-					          <label for="precio">Precio normal</label>
-					        </div>
-					        <div class="input-field col col-md-6">
-					          <input id="precio_especial" type="text" class="validate" required>
-					          <label for="precio_especial">Precio rebajado</label>
-					        </div>
-					      </div>
-					     
-					      <div class="row">
 					        <div class="col s6">
 					          <p>
 							      <input type="checkbox" id="habilitado" checked="checked"/>
@@ -46,6 +46,42 @@
 					        </div>
 					      </div>
 					      
+					    </form>
+				    </div>
+
+				  </div>
+				</div>
+				<div class="card">
+				  <div class="card-body">
+				    <h5 class="card-title">Boletos</h5>
+				    <div class="card-text">
+				    	<form class="col s12">
+					      <div class="row">
+					        <div class="input-field col col-md-8">
+					          <input id="boletos" type="text" class="validate" required>
+					          <label for="boletos">Número de boletos</label>
+					        </div>
+					        <div class="input-field col col-md-4">
+					          <input id="minimo" type="text" class="validate" required>
+					          <label for="minimo">Cantidad minima de boletos a vender</label>
+					        </div>
+					      </div>
+					      <div class="row">
+					        <div class="input-field col s12">
+					          <input id="fecha_limite" type="text" class="validate datepicker" required>
+					          <label for="fecha_limite">Fecha limite</label>
+					        </div>
+					      </div>
+					      <div class="row">
+					        <div class="input-field col col-md-6">
+					          <input id="precio" type="text" class="validate" required>
+					          <label for="precio">Precio normal</label>
+					        </div>
+					        <div class="input-field col col-md-6">
+					          <input id="precio_especial" type="text" class="validate" required>
+					          <label for="precio_especial">Precio rebajado</label>
+					        </div>
+					      </div>					      
 					    </form>
 				    </div>
 
@@ -69,16 +105,16 @@
 					      <div class="row">
 					        <div class="col s6">
 					          <p>
-							      <input type="checkbox" id="cat1"/>
-							      <label for="cat1">Celulares</label>
+							      <input type="checkbox" id="cat2"/>
+							      <label for="cat2">Celulares</label>
 						      </p>
 					        </div>
 					      </div>
 					      <div class="row">
 					        <div class="col s6">
 					          <p>
-							      <input type="checkbox" id="cat1"/>
-							      <label for="cat1">Videojuegos</label>
+							      <input type="checkbox" id="cat3"/>
+							      <label for="cat3">Videojuegos</label>
 						      </p>
 					        </div>
 					      </div>
