f53e149e515e4e1f23b93f3e07683f1e86eede6a
Alexis Morales <alx.morales@hadoukendev.com>
2018-02-19 07:55:19 -0600

    Slug

--- database/seeds/Categorias.php
+++ database/seeds/Categorias.php
@@ -13,26 +13,33 @@ class Categorias extends Seeder
     {
         DB::table('categorias')->insert([
             'nombre'=>'Videojuegos',
+            'slug'=>'videojuegos',
         ]);
         DB::table('categorias')->insert([
             'nombre'=>'Celulares',
+            'slug'=>'celulares',
         ]);
         DB::table('categorias')->insert([
             'nombre'=>'Tabletas',
+            'slug'=>'tabletas',
         ]);
         DB::table('categorias')->insert([
             'nombre'=>'Relojes',
+            'slug'=>'relojes',
         ]);
         DB::table('categorias')->insert([
             'nombre'=>'Audio',
+            'slug'=>'audio',
         ]);
 
 
         DB::table('fuentes')->insert([
             'nombre'=>'Melate',
+            'slug'=>'melate',
         ]);
         DB::table('fuentes')->insert([
             'nombre'=>'Lotería Nacional',
+            'slug'=>'loteria-nacional',
         ]);
     }
 }
