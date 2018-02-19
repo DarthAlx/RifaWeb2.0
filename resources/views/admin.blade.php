543756b27af7b80fa7d700edb90f0fb83b495c29
Alexis Morales <alx.morales@hadoukendev.com>
2018-02-19 10:53:05 -0600

    desktop

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -104,8 +104,27 @@ var pdx = document.getElementById("productosbar").getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Mujeres", "Hombres"],
+        labels: ["Hombres","Mujeres" ],
         datasets: [{
+            label: '# Hombres',
+            data: [{{$hombres}}],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        },
+        {
             label: '# Mujeres',
             data: [{{$mujeres}}],
             backgroundColor: [
@@ -126,25 +145,7 @@ var myChart = new Chart(ctx, {
             ],
             borderWidth: 1
         },
-        {
-            label: '# Hombres',
-            data: [{{$hombres}}],
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        ]
     },
     options: {
     	legend: {
